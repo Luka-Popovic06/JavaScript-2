dfc76471b2a0ce4a90f4fd81a0b3a803df84dbdc
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-03 20:27:22 +0100

    function 1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+/*1*/
+
+function calculateDiscaunt(cena, popust) {
+  const calculate = `Cena je ${cena}, popust je ${popust}, cena sa popustom je ${
+    cena - cena * (popust / 100)
+  }`;
+  return calculate;
+}
+const racun = calculateDiscaunt(100, 20);
+console.log(racun);
